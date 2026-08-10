ce59c1cb4d116668ba3c3e443aa7657441e57470
Marcelo Félix <marcelofpfelix@gmail.com>
2026-08-10 15:11:41 +0100

    chore: use papyrus 0.2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import { loadPaperConfig } from "astro-theme-papyrus/config";
+import { loadPapyrusConfig } from "astro-theme-papyrus/config";
 import papyrus from "astro-theme-papyrus/integration";
 
-const paper = await loadPaperConfig();
+const papyrusConfig = await loadPapyrusConfig();
 
 export default defineConfig({
-  site: paper.site ?? "https://papyrus-template.local",
+  site: papyrusConfig.site ?? "https://papyrus-template.local",
   integrations: [papyrus(), sitemap()],
 });
